174f4693161c957cb70e243370a52045579d90a9
henni saaranen <n0sahe00@students.oamk.fi>
2022-09-27 19:35:30 +0300

    add to repo

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'; 
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [euros, setEuros] = useState('');
+  const [pounds, setPounds] = useState(0);
+
+  /* function calculate() {
+    const result = euros.replace(',', '.') * 0.9;
+    setPounds(result);
+  } */
+
+  function change(text) {
+    setEuros(text);
+    const result = text.replace(',', '.') * 0.9;
+    setPounds(result);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.field}>Euros</Text>
+      <TextInput 
+        style={styles.field}
+        value={euros}
+          onChangeText={text => change(text)}
+          keyboard='decimal-pad'>
+        </TextInput>
+        <Text style={styles.field}>Pounds</Text>
+        <Text style={styles.field}>{pounds.toFixed(2)}</Text>
+        {/* <Button onPress={calculate} title="Calculate"></Button>     */}
     </View>
   );
 }
@@ -16,5 +39,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 50,
+    marginLeft: 10,
   },
+  field: {
+    marginBottom: 10
+  }
 });
